378da81e98572b1c3d15bb9c4598d202d04cf04e
Tnalxmsk <alstnwkd990@naver.com>
2025-07-06 05:58:29 +0900

    :art: style: 기본 페이지 컴포넌트 추가

---
+++ src/pages/home/ui/HomePage.tsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+const HomePage = () => {
+  return <HomePageContainer>HomePage</HomePageContainer>;
+};
+
+export default HomePage;
+
+const HomePageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
