b54db5c50c6e0988dab617fc565288de8fa478a1
Tnalxmsk <alstnwkd990@naver.com>
2025-07-06 05:56:44 +0900

    :art: style: BaseColor 정의

---
+++ src/app/styles/colors.ts
@@ -0,0 +1,80 @@
+export const colors = {
+  lightMode: {
+    brand: {
+      primary: '#CAFF29',
+      dark: '#80CA14',
+      deepDark: '#00362E',
+    },
+    text: {
+      text1: '#000000',
+      text2: '#666666',
+      text3: '#666666',
+      text4: '#B2B2B2',
+      text5: '#CCCCCC',
+      text1color: '#CAFF29',
+      text1w: '#FFFFFF',
+    },
+    icon: {
+      icon: '#1D1D1D',
+      icon2: '#666666',
+      iconColor: '#CAFF29',
+      iconDisabled: '#B2B2B2',
+      iconW: '#FFFFFF',
+    },
+    background: {
+      bg1: '#FFFFFF',
+      bg2: '#F7F7F7',
+      bg3: '#FCFCFC',
+      bg4: '#F2F2F2',
+      bgColor: '#F5F7F7',
+    },
+    divider: {
+      divider1: '#CCCCCC',
+      divider2: '#B2B2B2',
+    },
+    semantic: {
+      success: '#0094F7',
+      warning: '#E7CD3F',
+      error: '#FF4C00',
+    },
+  },
+  darkMode: {
+    brand: {
+      primary: '#CAFF29',
+    },
+    text: {
+      text1: '#FFFFFF',
+      text2: '#B2B2B2',
+      text3: '#B2B2B2',
+      text4: '#666666',
+      text5: '#4D4D4D',
+      text1color: '#CAFF29',
+      text1w: '#FFFFFF',
+    },
+    icon: {
+      icon: '#FCFCFC',
+      icon2: '#808080',
+      iconColor: '#CAFF29',
+      iconDisabled: '#666666',
+      iconW: '#FFFFFF',
+    },
+    background: {
+      bg1: '#1D1D1D',
+      bg2: '#333333',
+      bg3: '#4D4D4D',
+      bg4: '#666666',
+      bgColor: '#333333',
+    },
+    divider: {
+      divider1: '#4D4D4D',
+      divider2: '#666666',
+    },
+    semantic: {
+      success: '#0094F7',
+      warning: '#E7CD3F',
+      error: '#FF4C00',
+    },
+  },
+};
+
+export type ColorsType = typeof colors;
