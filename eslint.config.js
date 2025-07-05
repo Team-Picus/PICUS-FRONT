7d8ad01d2eaa44ed3b1eae6a9d8f695776fe7068
Tnalxmsk <alstnwkd990@naver.com>
2025-07-06 04:29:05 +0900

    init: prettier 설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,20 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
+import react from 'eslint-plugin-react';
+import prettier from 'eslint-plugin-prettier';
 
 export default tseslint.config([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
+    plugins: {
+      react,
+      prettier,
+    },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
@@ -19,5 +25,8 @@ export default tseslint.config([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      'prettier/prettier': 'error',
+    },
   },
-])
+]);
