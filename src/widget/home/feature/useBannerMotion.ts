886ea78cee582e054c8471f17c9c1db1fc5c60c9
cjiyun <chlwldbs78@gmail.com>
2025-08-01 07:59:09 +0900

    :sparkles: feat: 홈 배너 클릭 인터랙션 추가

--- src/widget/home/feature/useBannerMotion.ts
+++ src/widget/home/feature/useBannerMotion.ts
@@ -1,18 +1,37 @@
 import { useAnimation } from 'framer-motion';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router';
 
 const useBannerMotion = () => {
+  const [isClicked, setIsClicked] = useState(false);
+  const navigate = useNavigate();
+
   const sectionControls = useAnimation();
   const leftControls = useAnimation();
   const rightControls = useAnimation();
+  const titleControls = useAnimation();
+  const descriptionControls = useAnimation();
+  const imageControls = useAnimation();
 
   useEffect(() => {
     async function seq() {
-      await sectionControls.start({
-        opacity: 1,
-        y: 0,
-        transition: { type: 'spring', stiffness: 300, damping: 80 },
-      });
+      await Promise.all([
+        sectionControls.start({
+          opacity: 1,
+          y: 0,
+          transition: { type: 'spring', stiffness: 300, damping: 80 },
+        }),
+        titleControls.start({
+          opacity: 1,
+          x: 0,
+          transition: { type: 'spring', stiffness: 300, damping: 80 },
+        }),
+        descriptionControls.start({
+          opacity: 1,
+          x: 0,
+          transition: { type: 'spring', stiffness: 300, damping: 80 },
+        }),
+      ]);
       await Promise.all([
         leftControls.start({
           opacity: 1,
@@ -28,9 +47,50 @@ const useBannerMotion = () => {
     }
 
     seq();
-  }, [sectionControls, leftControls, rightControls]);
+  }, [sectionControls, leftControls, rightControls, titleControls, descriptionControls]);
 
-  return { sectionControls, leftControls, rightControls };
+  const handleClick = async () => {
+    if (isClicked) {
+      navigate('/weekly_magazine');
+      return;
+    }
+
+    await Promise.all([
+      imageControls.start({
+        opacity: 1,
+        y: -112,
+        height: '573px',
+        clipPath: 'inset(0%)',
+        transition: { duration: 1, type: 'spring', stiffness: 300, damping: 80 },
+      }),
+      titleControls.start({
+        opacity: 0,
+        transition: { duration: 0.6 },
+      }),
+      descriptionControls.start({
+        opacity: 0,
+        transition: { duration: 0.6 },
+      }),
+    ]);
+
+    setIsClicked(true);
+
+    await Promise.all([
+      titleControls.start({ opacity: 1, transition: { duration: 0.6 } }),
+      descriptionControls.start({ opacity: 1, transition: { duration: 0.6 } }),
+    ]);
+  };
+
+  return {
+    isClicked,
+    handleClick,
+    sectionControls,
+    leftControls,
+    rightControls,
+    titleControls,
+    descriptionControls,
+    imageControls,
+  };
 };
 
 export default useBannerMotion;
--- src/widget/home/ui/MainBanner.tsx
+++ src/widget/home/ui/MainBanner.tsx
@@ -1,73 +1,87 @@
 import styled from '@emotion/styled';
-import { motion } from 'framer-motion';
+import { motion, type HTMLMotionProps } from 'framer-motion';
 import ImgMainbannerEx from '@image/img-mainbanner-ex.png';
 import useBannerMotion from '@widget/home/feature/useBannerMotion.ts';
 
 const MainBanner = () => {
-  const { sectionControls, rightControls, leftControls } = useBannerMotion();
+  const {
+    isClicked,
+    handleClick,
+    sectionControls,
+    rightControls,
+    leftControls,
+    titleControls,
+    descriptionControls,
+    imageControls,
+  } = useBannerMotion();
 
   return (
-    <MainBannerContainer>
-      {/* 타이틀 */}
-      <MainBannerTitleSection>
-        <MainBannerTitle
-          initial={{ opacity: 1, x: -200 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ type: 'spring', stiffness: 300, damping: 80 }}
+    <>
+      <MainBannerContainer key="notClicked" isOverview={isClicked}>
+        <MainBannerTitleSection style={{ order: isClicked ? 1 : 0 }} isOverview={isClicked}>
+          <MainBannerTitle initial={{ opacity: 1, x: -200 }} animate={titleControls}>
+            <span>시선</span>
+            <span>No.1_1w July</span>
+          </MainBannerTitle>
+          <MainBannerDate initial={{ opacity: 1, x: 200 }} animate={titleControls}>
+            7월 1주차
+          </MainBannerDate>
+        </MainBannerTitleSection>
+        {/* 배너 이미지 */}
+        <MainBannerImageSection
+          layoutId="main-banner-image"
+          initial={{ opacity: 1, y: -500 }}
+          animate={isClicked ? imageControls : sectionControls}
+          exit={{ opacity: 1 }}
+          isOverview={isClicked}
+          onClick={handleClick}
         >
-          <span>시선</span>
-          <span>No.1_1w Junly</span>
-        </MainBannerTitle>
-        <MainBannerDate
-          initial={{ opacity: 1, x: 200 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ type: 'spring', stiffness: 300, damping: 80 }}
-        >
-          7월 1주차
-        </MainBannerDate>
-      </MainBannerTitleSection>
-      {/* 배너 이미지 */}
-      <MainBannerImageSection initial={{ opacity: 1, y: -500 }} animate={sectionControls}>
-        <HalfBGLeft initial={{ opacity: 1, x: 0 }} animate={leftControls} />
-        <HalfBGRight initial={{ opacity: 1, x: 0 }} animate={rightControls} />
-        <MainBannerImage src={ImgMainbannerEx} alt="" />
-      </MainBannerImageSection>
-      {/* 설명 */}
-      <MainBannerDescriptionSection>
-        <MainBannerDescriptionBold
-          initial={{ opacity: 1, x: -500 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ type: 'spring', stiffness: 300, damping: 80 }}
-        >
-          {'여름의 나른함을\n담아내는 작가들'}
-        </MainBannerDescriptionBold>
-        <MainBannerDescriptionSub
-          initial={{ opacity: 1, x: 500 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ type: 'spring', stiffness: 300, damping: 80 }}
-        >
-          {`모든 사진에는 그 순간의 진실이 담겨\n있습니다. 빛과 그림자의 균형 속에서\n감정을 포착합니다.`}
-        </MainBannerDescriptionSub>
-      </MainBannerDescriptionSection>
-    </MainBannerContainer>
+          <HalfBGLeft initial={{ opacity: 1, x: 0 }} animate={leftControls} />
+          <HalfBGRight initial={{ opacity: 1, x: 0 }} animate={rightControls} />
+          <MainBannerImage src={ImgMainbannerEx} alt="" />
+          <Overlay />
+        </MainBannerImageSection>
+        {/* 설명 */}
+        <MainBannerDescriptionSection style={{ order: isClicked ? 2 : 0 }}>
+          <MainBannerDescriptionBold
+            initial={{ opacity: 1, x: -500 }}
+            animate={descriptionControls}
+            isOverview={isClicked}
+          >
+            {isClicked ? '여름의 나른함을 담아내는 작가들' : '여름의 나른함을\n담아내는 작가들'}
+          </MainBannerDescriptionBold>
+          <MainBannerDescriptionSub
+            initial={{ opacity: 1, x: 500 }}
+            animate={descriptionControls}
+            isOverview={isClicked}
+          >
+            {`모든 사진에는 그 순간의 진실이 담겨\n있습니다. 빛과 그림자의 균형 속에서\n감정을 포착합니다.`}
+          </MainBannerDescriptionSub>
+        </MainBannerDescriptionSection>
+      </MainBannerContainer>
+    </>
   );
 };
 
 export default MainBanner;
 
-const MainBannerContainer = styled.div`
+export const MainBannerContainer = styled(motion.div)<
+  HTMLMotionProps<'div'> & { isOverview: boolean }
+>`
   display: flex;
   flex-direction: column;
-  padding: 32px 16px;
-  gap: 56px;
+  padding: ${({ isOverview }) => (isOverview ? '0' : '32px 16px')};
+  gap: ${({ isOverview }) => (isOverview ? '16px' : '56px')};
 `;
 
-const MainBannerTitleSection = styled.div`
+export const MainBannerTitleSection = styled.div<{ isOverview: boolean }>`
   display: flex;
   justify-content: space-between;
+  height: 24px;
+  padding: ${({ isOverview }) => (isOverview ? '0px 16px' : '0px')};
 `;
 
-const MainBannerTitle = styled(motion.div)`
+export const MainBannerTitle = styled(motion.div)`
   span:first-of-type {
     font: ${({ theme }) => theme.fonts.labelMB};
     font-size: 16px;
@@ -81,52 +95,55 @@ const MainBannerTitle = styled(motion.div)`
   }
 `;
 
-const MainBannerDate = styled(motion.div)`
+export const MainBannerDate = styled(motion.div)`
   font: ${({ theme }) => theme.fonts.labelM};
   color: ${({ theme }) => theme.colors.lightMode.text.text1};
 `;
 
-const MainBannerDescriptionSection = styled.div`
+export const MainBannerDescriptionSection = styled.div`
   display: flex;
   flex-direction: column;
   gap: 12px;
 `;
 
-const MainBannerDescriptionBold = styled(motion.div)`
-  font: ${({ theme }) => theme.fonts.title1};
+export const MainBannerDescriptionBold = styled(motion.div)<{ isOverview: boolean }>`
+  font: ${({ theme, isOverview }) => (isOverview ? theme.fonts.title3 : theme.fonts.title1)};
   color: ${({ theme }) => theme.colors.lightMode.text.text1};
   white-space: pre-wrap;
   margin-right: auto;
+  padding: ${({ isOverview }) => (isOverview ? '0px 16px' : '0px')};
 `;
 
-const MainBannerDescriptionSub = styled(motion.div)`
+export const MainBannerDescriptionSub = styled(motion.div)<{ isOverview: boolean }>`
   font: ${({ theme }) => theme.fonts.body4};
-  font-size: 14px;
+  font-size: ${({ isOverview }) => (isOverview ? '13px' : '14px')};
   line-height: 160%;
   color: #7b7b7b;
   white-space: pre-wrap;
-  margin-left: auto;
+  margin-left: ${({ isOverview }) => (isOverview ? 'none' : 'auto')};
+  padding: ${({ isOverview }) => (isOverview ? '0px 16px' : '0px')};
 `;
 
-const MainBannerImageSection = styled(motion.div)`
+export const MainBannerImageSection = styled(motion.div)<{ isOverview: boolean }>`
   position: relative;
-  height: 573px;
-  clip-path: ellipse(100% 50% at 50% 50%);
+  height: ${({ isOverview }) => (isOverview ? '742px' : '573px')};
+  clip-path: ${({ isOverview }) => (isOverview ? 'none' : 'ellipse(100% 50% at 50% 50%)')};
   overflow: hidden;
+  cursor: pointer;
 
   @media (max-width: 440px) {
-    height: 282px;
+    height: ${({ isOverview }) => (isOverview ? '380px' : '282px')};
   }
 `;
 
-const MainBannerImage = styled.img`
+export const MainBannerImage = styled.img`
   width: 100%;
   height: 100%;
   object-fit: fill;
   object-position: center;
 `;
 
-const HalfBGLeft = styled(motion.div)`
+export const HalfBGLeft = styled(motion.div)`
   position: absolute;
   left: 0;
   top: 0;
@@ -138,7 +155,7 @@ const HalfBGLeft = styled(motion.div)`
   clip-path: polygon(0 0, 100% 100%, 100% 100%, 0 100%);
 `;
 
-const HalfBGRight = styled(motion.div)`
+export const HalfBGRight = styled(motion.div)`
   position: absolute;
   right: 0;
   top: 0;
@@ -148,4 +165,22 @@ const HalfBGRight = styled(motion.div)`
   background: ${({ theme }) => theme.colors.lightMode.brand.primary};
   z-index: 1;
   clip-path: polygon(0 0, 100% 0, 100% 0%, 100% 100%);
+`;
+
+export const Overlay = styled.div`
+  position: absolute;
+  top: 595px;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  height: 148px;
+  background: linear-gradient(180deg, rgba(128, 202, 20, 0) 0%, #80ca14 100%);
+  opacity: 0.3;
+  transition: opacity 0.3s ease;
+  z-index: 1;
+  pointer-events: none;
+
+  @media (max-width: 440px) {
+    top: 232px;
+  }
 `;
