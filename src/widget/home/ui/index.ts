04ee23e626478bbfd8b0e63614317b6cd4df707c
cjiyun <chlwldbs78@gmail.com>
2025-08-01 09:58:47 +0900

    :truck: rename: PictureWorkList -> PictureWorkCategory로 이름 변경

--- src/widget/home/ui/index.ts
+++ src/widget/home/ui/index.ts
@@ -1,5 +1,5 @@
 export { default as DiscoveryBanner } from './DiscoveryBanner.tsx';
 export { default as MainBanner } from './MainBanner.tsx';
-export { default as PictureWorkList } from './PictureWorkList.tsx';
+export { default as PictureWorkCategory } from './PictureWorkCategory.tsx';
 export { default as HomeFooter } from './HomeFooter.tsx';
 export * as S from './Home.styles.ts';
