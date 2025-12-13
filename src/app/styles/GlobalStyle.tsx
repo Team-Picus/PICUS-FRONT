8751d13962a8079939a6808dd630cd4847b286f9
cjiyun <chlwldbs78@gmail.com>
2025-12-13 21:40:23 +0900

    :art: style: GlobalStyle button 상태(hover/focus/active) border 제거

--- src/app/styles/GlobalStyle.tsx
+++ src/app/styles/GlobalStyle.tsx
@@ -39,13 +39,16 @@ const GlobalStyle = () => {
           outline: none;
 
           &:hover {
+            border: none;
             outline: none;
           }
 
           &:focus {
+            border: none;
             outline: none;
           }
           &:active {
+            border: none;
             outline: none;
           }
         }
