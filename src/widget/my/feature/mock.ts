7febb8cb2fef2080cac2173253b317da4d0f4ad9
cjiyun <chlwldbs78@gmail.com>
2025-12-15 13:44:28 +0900

    :wrench: chore: 작가 승인 요청 mock 데이터 추가

--- src/widget/my/feature/mock.ts
+++ src/widget/my/feature/mock.ts
@@ -1,4 +1,5 @@
 import ImgProfileEx from '@image/img-profile-ex.png';
+import type { ApprovalRequestDTO } from '@widget/my/types/approvalForm.ts';
 
 interface User {
   name: string;
@@ -22,4 +23,38 @@ export const user: User = {
   reservation_count: 0,
   following_count: 0,
   moodboard_count: 0,
+};
+
+export const approvalRequestMock: ApprovalRequestDTO = {
+  activity_duration: '3개월',
+  projects: [
+    {
+      project_name: '개인스튜디오운영',
+      start_date: '2025-04-01T10:00:00',
+      end_date: '2025-05-02T18:00:00',
+    },
+    {
+      project_name: '무신사 룩북',
+      start_date: '2024-10-10T09:30:00',
+      end_date: '2024-12-12T19:00:00',
+    },
+  ],
+  activity_area: ['서울시 송파구', '성남시 수정구'],
+  skills: [
+    { skill_type: 'CAMERA', content: '카메라 기종 1' },
+    { skill_type: 'CAMERA', content: '카메라 기종 2' },
+    { skill_type: 'LIGHT', content: '조명 기종 1' },
+    { skill_type: 'EDIT', content: 'Adobe Photoshop' },
+  ],
+  studio: {
+    studio_name: '스튜디오 시선',
+    employees_count: 3,
+    business_hours: '00:00-00:00 00:00-00:00 (Break Time)',
+    address: '서울시 송파구 00동 00길 182-17',
+  },
+  portfolio_links: [
+    'https://www.instagram.com/your_handle',
+    'https://behance.net/your_portfolio',
+    'https://yourdomain.com',
+  ],
 };
