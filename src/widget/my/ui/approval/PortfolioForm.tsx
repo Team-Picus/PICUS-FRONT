dba3b654f552e378f2bf573ed0fbd5cfdab0f10e
cjiyun <chlwldbs78@gmail.com>
2025-12-15 14:15:38 +0900

    :sparkles: feat: 작가 승인 포트폴리오 작성 폼 컴포넌트 구현 및 index export 추가

---
+++ src/widget/my/ui/approval/PortfolioForm.tsx
@@ -0,0 +1,26 @@
+import SectionHeader from '@shared/components/SectionHeader.tsx';
+import TextField from '@shared/components/TextField.tsx';
+import { S } from '@widget/my/ui/approval';
+import SelectMenuOption from '@shared/components/SelectMenuOption.tsx';
+
+const PortfolioForm = () => {
+  return (
+    <S.SectionContainer>
+      <SectionHeader text="포트폴리오 링크" />
+      <S.AddableField>
+        <TextField
+          type="text"
+          states="default"
+          isLabel
+          isTextLength={false}
+          isHelpText={false}
+          label="분야"
+          placeholder="https://"
+        />
+        <SelectMenuOption text="추가하기" />
+      </S.AddableField>
+    </S.SectionContainer>
+  );
+};
+
+export default PortfolioForm;
--- src/widget/my/ui/approval/index.ts
+++ src/widget/my/ui/approval/index.ts
@@ -1,3 +1,4 @@
 export { default as OrganizationForm } from './OrganizationForm.jsx';
 export { default as ProfileForm } from './ProfileForm.jsx';
+export { default as PortfolioForm } from './PortfolioForm.jsx';
 export * as S from './approval.styles.ts';
