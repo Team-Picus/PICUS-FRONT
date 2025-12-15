d7292fc719251b567a09ef9b73ceb158986219a1
cjiyun <chlwldbs78@gmail.com>
2025-12-15 14:19:18 +0900

    :recycle: refactor: approval 모듈 index에 ApprovalOverview, ProgressBar export 추가

--- src/widget/my/ui/approval/index.ts
+++ src/widget/my/ui/approval/index.ts
@@ -1,3 +1,5 @@
+export { default as ApprovalOverview } from './ApprovalOverview.jsx';
+export { default as ProgressBar } from './ProgressBar.jsx';
 export { default as OrganizationForm } from './OrganizationForm.jsx';
 export { default as ProfileForm } from './ProfileForm.jsx';
 export { default as PortfolioForm } from './PortfolioForm.jsx';
