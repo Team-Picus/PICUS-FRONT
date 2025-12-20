8e46c4d40321f4b554a9f6f2638a76d48486a884
cjiyun <chlwldbs78@gmail.com>
2025-12-20 18:26:38 +0900

    :wrench: chore: close-big, progress 아이콘 tsx 파일 추가

---
+++ src/shared/assets/icon/ic-close-big.tsx
@@ -0,0 +1,26 @@
+const IcCloseBig = () => {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M3.5 3.5L9.49926 9.49926"
+        stroke="currentColor"
+        strokeWidth="1.3"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="M11.4961 11.4961L20 20"
+        stroke="currentColor"
+        strokeWidth="1.3"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="M19.5009 3.5L3.5 19.5009"
+        stroke="currentColor"
+        strokeWidth="1.3"
+        strokeMiterlimit="10"
+      />
+    </svg>
+  );
+};
+
+export default IcCloseBig;
