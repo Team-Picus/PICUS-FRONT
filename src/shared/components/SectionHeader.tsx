971ca602310f94cbde5dc3f51ddd4f00c9e6bded
cjiyun <chlwldbs78@gmail.com>
2025-12-22 05:45:40 +0900

    :sparkles: feat: SectionHeader size(M/L) prop 추가

--- src/shared/components/SectionHeader.tsx
+++ src/shared/components/SectionHeader.tsx
@@ -1,18 +1,21 @@
 import styled from '@emotion/styled';
 
+type LabelSize = 'M' | 'L';
+
 interface SectionHeaderProps {
   text: string;
+  size?: LabelSize;
 }
 
-const SectionHeader = ({ text }: SectionHeaderProps) => {
-  return <Header>{text}</Header>;
+const SectionHeader = ({ text, size = 'L' }: SectionHeaderProps) => {
+  return <Header $size={size}>{text}</Header>;
 };
 
 export default SectionHeader;
 
-const Header = styled.div`
+const Header = styled.div<{ $size?: LabelSize }>`
   position: relative;
-  font: ${({ theme }) => theme.fonts.labelL};
+  font: ${({ $size, theme }) => ($size === 'L' ? theme.fonts.labelL : theme.fonts.labelMB)};
   color: ${({ theme }) => theme.colors.lightMode.text.text3};
   padding-left: 12px;
 
