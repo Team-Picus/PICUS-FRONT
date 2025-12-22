d05c05fb76eb571700f6f8f284f336b270031782
cjiyun <chlwldbs78@gmail.com>
2025-12-22 11:43:02 +0900

    :recycle: refactor: 작가 승인 페이지 ProfileForm에서 보유기술 섹션 컴포넌트 분리

--- src/widget/my/ui/approval/ProfileForm.tsx
+++ src/widget/my/ui/approval/ProfileForm.tsx
@@ -2,6 +2,7 @@ import SectionHeader from '@shared/components/SectionHeader.tsx';
 import TextField from '@shared/components/TextField.tsx';
 import SelectMenuOption from '@shared/components/SelectMenuOption.tsx';
 import { S } from '@widget/my/ui/approval';
+import SkillsForm from '@widget/my/component/SkillsForm.tsx';
 
 const ProfileForm = () => {
   return (
@@ -18,29 +19,7 @@ const ProfileForm = () => {
           <SelectMenuOption text="추가하기" />
         </S.AddableField>
       </S.SectionContainer>
-      <S.SectionContainer>
-        <SectionHeader text="보유기술" />
-        <S.AddableField>
-          <TextField showLabel label="카메라" placeholder="사용 중인 카메라 기종을 입력해주세요." />
-          <SelectMenuOption text="추가하기" />
-        </S.AddableField>
-        <S.AddableField>
-          <TextField
-            showLabel
-            label="조명"
-            placeholder="사용하는 조명 장비나 방식이 있다면 적어주세요."
-          />
-          <SelectMenuOption text="추가하기" />
-        </S.AddableField>
-        <S.AddableField>
-          <TextField
-            showLabel
-            label="편집"
-            placeholder="사용하는 편집 툴이나 작업 스타일을 입력해주세요."
-          />
-          <SelectMenuOption text="추가하기" />
-        </S.AddableField>
-      </S.SectionContainer>
+      <SkillsForm />
     </>
   );
 };
